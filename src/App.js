62ea78ba3a9757aa708de65a665d8dda9cbe9929
Mike Fritzsche <mikef65@gmail.com>
2021-09-09 20:56:00 -0700

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Link, Route, Switch } from 'react-router-dom'
 
 import './App.css'
-import axios from 'axios'
-
-import Signup from './containers/Signup'
-import Users from './containers/Users'
-
-window.axios = axios
-
-/* Home component */
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
 
 function App() {
   return (
@@ -24,21 +11,21 @@ function App() {
         <h3>Forging ideas into reality</h3>
       </header>
 
-      <nav className="navbar navbar-light">
-        <ul className="nav navbar-nav">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/users">Users</Link></li>
-        </ul>
-      </nav>
+      {/*<nav className="navbar navbar-light">*/}
+      {/*  <ul className="nav navbar-nav">*/}
+      {/*    <li><Link to="/">Home</Link></li>*/}
+      {/*    <li><Link to="/signup">Sign Up</Link></li>*/}
+      {/*    <li><Link to="/users">Users</Link></li>*/}
+      {/*  </ul>*/}
+      {/*</nav>*/}
 
-      <div style={{ padding: 20 }}>
-        <Switch>
-          <Route path="/" component={Home} exact/>
-          <Route path="/signup" component={Signup}/>
-          <Route path="/users" component={Users}/>
-        </Switch>
-      </div>
+      {/*<div style={{ padding: 20 }}>*/}
+      {/*  <Switch>*/}
+      {/*    <Route path="/" component={Home} exact/>*/}
+      {/*    <Route path="/signup" component={Signup}/>*/}
+      {/*    <Route path="/users" component={Users}/>*/}
+      {/*  </Switch>*/}
+      {/*</div>*/}
     </div>
   )
 }
